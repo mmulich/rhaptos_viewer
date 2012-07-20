839403fb3a16454f00a45db0abf42e0b42757b84
Michael Mulich <michael.mulich@gmail.com>
2012-07-20 13:53:53 -0400

    Remove boilerplate views

--- rhaptos_viewer/tests.py
+++ rhaptos_viewer/tests.py
@@ -8,9 +8,3 @@ class ViewTests(unittest.TestCase):
 
     def tearDown(self):
         testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'rhaptos_viewer')
