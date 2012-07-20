24b7ae12c9b46404c6213ac043f9dcede6bc65c5
Michael Mulich <michael.mulich@gmail.com>
2012-07-20 14:37:19 -0400

    Basic home page

--- rhaptos_viewer/__init__.py
+++ rhaptos_viewer/__init__.py
@@ -8,5 +8,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('pyramid_jinja2')
+    config.add_jinja2_search_path('rhaptos_viewer:templates/')
+    config.add_route('casa', '/')
     config.scan()
     return config.make_wsgi_app()
--- rhaptos_viewer/views.py
+++ rhaptos_viewer/views.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 from pyramid.view import view_config
+
+
+SITE_TITLE = 'Connexions Web Viewer'
+
+@view_config(route_name='casa', renderer='casa.jinja2')
+def casa(request):
+    """The home page for this application."""
+    return {'title': SITE_TITLE}
